b9b684edb8dd9ad251e9b87d7dcf98f9dc13cc41
marius <marius.vuscan@gmail.com>
2019-03-15 22:18:11 +0200

    done encription algorithm

--- PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace PasswordGenerator
 {
@@ -20,6 +22,7 @@ namespace PasswordGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        SlotsCreator s1;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +38,8 @@ namespace PasswordGenerator
             bool similar = (bool)SimilarCheckBox.IsChecked;
             bool ambiguous = (bool)AmbiguousCheckBox.IsChecked;
 
-            ResultTextBlock.Text = generatePassword(length,symbols,numbers,lowercase,uppercase,similar,ambiguous);
+            ResultTextBlock.Text = generatePassword(length, symbols, numbers, lowercase, uppercase, similar, ambiguous);
+            s1.updatePassWordSet(ResultTextBlock.Text);
         }
 
         private string generatePassword(int length, bool symbols, bool numbers, bool lowercase, bool uppercase, bool similar, bool ambiguous)
@@ -46,7 +50,7 @@ namespace PasswordGenerator
             {
                 char c = (char)rnd.Next(33, 127);//generate random ascii symbol
 
-                while(!symbols && Char.IsSymbol(c) || !numbers && Char.IsNumber(c) || !lowercase && Char.IsLower(c) ||
+                while (!symbols && Char.IsSymbol(c) || !numbers && Char.IsNumber(c) || !lowercase && Char.IsLower(c) ||
                     !uppercase && Char.IsUpper(c) || similar && "il1Lo0O".Contains(c) || ambiguous && "{}[]()/\\'\"`~,;:.<>".Contains(c))
                 {
                     c = (char)rnd.Next(33, 127);
@@ -55,6 +59,80 @@ namespace PasswordGenerator
                 password += c;
             }
             return password;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ResultTextBlock.Text.Length != int.Parse(PasswordLengthTextBox.Text))
+                {
+                    MessageBox.Show("Error! Please generate a password first!");
+                }
+                else
+                {
+                    TextWriter tw = new StreamWriter(@"../../TextFile1.txt", true);
+
+                    string original = ResultTextBlock.Text;
+
+                    byte[] encrypted = AesEncrypt.EncryptStringToBytes_Aes(original, key, iv);
+
+                    string str = "";
+
+                    for (int i = 0; i < encrypted.Length; i++)
+                    {
+                        str += (char)encrypted[i];
+                    }
+                    MessageBox.Show(str);
+
+                    tw.WriteLine(str);
+                    
+                    tw.Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error! File exception!");
+            }
+        }
+
+        byte[] iv = new byte[] { 1, 1, 43, 6, 23, 76, 34, 67, 23, 65, 24, 65, 23, 56, 34, 65 };
+        byte[] key = new byte[] { 1, 1, 43, 6, 23, 76, 34, 67, 23, 65, 24, 65, 23, 56, 34, 65, 1, 1, 43, 6, 23, 76, 34, 67, 23, 65, 24, 65, 23, 56, 34, 65 };
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> lastPasswords = new List<string>();
+            try
+            {
+                TextReader tr = new StreamReader(@"../../TextFile1.txt", true);
+                string aux = "";
+                while ((aux = tr.ReadLine()) != null)
+                {
+                    byte[] encryptedData = new byte[aux.Length];
+                    for (int i = 0; i < aux.Length; i++)
+                    {
+                        encryptedData[i] = (byte)aux[i];
+                    }
+                    string roundtrip = AesEncrypt.DecryptStringFromBytes_Aes(encryptedData, key, iv);
+                    lastPasswords.Add(roundtrip);
+                    
+                    foreach (var item in lastPasswords)
+                    {
+                        //LoadAndStatusListBox.Items.Add(item);
+                    }
+                }
+                tr.Close();
+
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error! File exception!");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            s1 = new SlotsCreator(9, DataStackPanel);
         }
     }
 }
---
+++ PasswordGenerator/PasswordGenerator/SlotsCreator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PasswordGenerator
+{
+    class SlotsCreator
+    {
+        static TextBox[,] dataArray;
+        private int numberOfElements;
+        private string passWordSet="";
+        private int index = 0;
+
+        public SlotsCreator(int numberOfElements, StackPanel parentElement)
+        {
+            this.numberOfElements = numberOfElements;
+            dataArray = new TextBox[numberOfElements, 3];
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                generateSlot(parentElement);
+            }
+        }
+
+        public void generateSlot(StackPanel parentElement)
+        {
+            //grid
+            Grid newGrid = new Grid();
+
+            //column Definition
+            for (int i = 0; i < 5; i++)
+            {
+                ColumnDefinition newCD = new ColumnDefinition();
+                newCD.Width = new GridLength(1, GridUnitType.Star);
+                newGrid.ColumnDefinitions.Add(newCD);
+            }
+
+            //add elements
+            TextBox titleTextBox = new TextBox();
+            TextBox userTextBox = new TextBox();
+            TextBox passTextBox = new TextBox();
+            Button useGeneratedButton = new Button();
+            useGeneratedButton.Content = "Use Generated";
+            useGeneratedButton.Click += delegate
+            {
+                passTextBox.Text = passWordSet;
+            };
+            Button copyPassButton = new Button();
+            copyPassButton.Content = "Copy Password in Clipboard";
+            copyPassButton.Click += delegate
+            {
+                Clipboard.SetText(passTextBox.Text);
+            };
+
+            //add data in array
+            dataArray[index, 0] = titleTextBox;
+            dataArray[index, 1] = userTextBox;
+            dataArray[index, 2] = passTextBox;
+
+            //set column for elements
+            Grid.SetColumn(titleTextBox, 0);
+            Grid.SetColumn(userTextBox, 1);
+            Grid.SetColumn(passTextBox, 2);
+            Grid.SetColumn(useGeneratedButton, 3);
+            Grid.SetColumn(copyPassButton, 4);
+
+            //append elements
+            newGrid.Children.Add(titleTextBox);
+            newGrid.Children.Add(userTextBox);
+            newGrid.Children.Add(passTextBox);
+            newGrid.Children.Add(useGeneratedButton);
+            newGrid.Children.Add(copyPassButton);
+
+            //add grid to main stackpanel
+            parentElement.Children.Add(newGrid);
+
+            index++;
+        }
+
+        public void updatePassWordSet(string newString)
+        {
+            passWordSet = newString;
+        }
+    }
+}
